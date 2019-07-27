a38ec77607c549e223d13fab3391ed27d024c234
kiranp79 <kiranpatel72091@gmail.com>
2019-07-27 17:22:01 +0530

    All APIs and web app with database connection

---
+++ WebApp/TestApi/LocationApi/Models/DataManager/LocationMaster.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LocationApi.Models;
+using LocationApi.Models.Repository;
+
+namespace LocationApi.Models.DataManager
+{
+    public class LocationMaster : IDataRepository<LocationInfo>
+    {
+        readonly LocationContext _LocationContext;
+
+        public LocationMaster(LocationContext context)
+        {
+            _LocationContext = context;
+        }
+        public IEnumerable<LocationInfo> GetAll()
+        {
+            return _LocationContext.Location.ToList();
+        }
+        //public LocationData Get(long id)
+        //{
+        //    return _LocationContext.Locations
+        //          .FirstOrDefault(o => o.LocationId = id);
+        //}
+        public void Add(LocationInfo entity)
+        {
+            _LocationContext.Location.Add(entity);
+            _LocationContext.SaveChanges();
+        }
+
+        public void Update(LocationInfo Location, LocationInfo entity)
+        {
+            Location.LocationID = entity.LocationID;
+            Location.FleetID = entity.FleetID;
+            Location.Latitude = entity.Latitude;
+            Location.Longitude = entity.Longitude;
+
+            _LocationContext.SaveChanges();
+        }
+        public void Delete(LocationInfo Location)
+        {
+            _LocationContext.Location.Remove(Location);
+            _LocationContext.SaveChanges();
+        }
+
+        public LocationInfo Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        LocationInfo IDataRepository<LocationInfo>.Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
